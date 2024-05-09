ea9d22519a3d7bed16dbcdad482db777b87fe218
Pablo Fondevilla <pfondevi@gmail.com>
2024-05-09 12:23:28 +0200

    Aun nada significativo

--- implementacion/AgentBusqueda.py
+++ implementacion/AgentBusqueda.py
@@ -15,16 +15,20 @@ Asume que el agente de registro esta en el puerto 9000
 """
 from os import getcwd, path
 import sys
+
+import requests
 sys.path.append(path.dirname(getcwd()))
 from multiprocessing import Process, Queue
 import socket
 import argparse
-import logging
+from AgentUtil.Logging import config_logger
 
 from rdflib import Namespace, Graph
-from flask import Flask
+from flask import Flask, request
 from AgentUtil.FlaskServer import shutdown_server
 from AgentUtil.Agent import Agent
+from AgentUtil.Util import gethostname
+from AgentUtil.ACLMessages import *
 from docs.ecsdi import ECSDI
 
 __author__ = 'javier'
@@ -40,8 +44,7 @@ parser.add_argument('--dir', default=None, help="Direccion del servicio de direc
 args = parser.parse_args()
 
 if not args.verbose:
-    log = logging.getLogger('werkzeug')
-    log.setLevel(logging.ERROR)
+    logger = config_logger(1, 'busqueda')
 
 # Configuration stuff
 if args.port is None:
@@ -93,11 +96,62 @@ app = Flask(__name__)
 @app.route("/comm")
 def comunicacion():
     """
-    Entrypoint de comunicacion
+    Entrypoint de comunicacion del agente
+    Simplemente retorna un objeto fijo que representa una
+    respuesta a una busqueda de hotel
+
+    Asumimos que se reciben siempre acciones que se refieren a lo que puede hacer
+    el agente (buscar con ciertas restricciones, reservar)
+    Las acciones se mandan siempre con un Request
+    Prodriamos resolver las busquedas usando una performativa de Query-ref
     """
     global dsgraph
     global mss_cnt
-    pass
+
+    logger.info('Peticion de informacion recibida')
+
+    # Extraemos el mensaje y creamos un grafo con el
+    message = request.args['content']
+    gm = Graph()
+    gm.parse(data=message, format='xml')
+
+    msgdic = get_message_properties(gm)
+
+    # Comprobamos que sea un mensaje FIPA ACL
+    if msgdic is None:
+        # Si no es, respondemos que no hemos entendido el mensaje
+        gr = build_message(
+            Graph(), ACL['not-understood'], sender=AgenteBusqueda.uri, msgcnt=mss_cnt)
+    else:
+        # Obtenemos la performativa
+        perf = msgdic['performative']
+
+        if perf != ACL.request:
+            # Si no es un request, respondemos que no hemos entendido el mensaje
+            gr = build_message(
+                Graph(), ACL['not-understood'], sender=AgenteBusqueda.uri, msgcnt=mss_cnt)
+        else:
+            # Extraemos el objeto del contenido que ha de ser una accion de la ontologia de acciones del agente
+            # de registro
+
+            # Averiguamos el tipo de la accion
+            if 'content' in msgdic:
+                content = msgdic['content']
+                accion = gm.value(subject=content, predicate=RDF.type)
+
+            # Aqui realizariamos lo que pide la accion
+            # Por ahora simplemente retornamos un Inform-done
+            gr = build_message(Graph(),
+                               ACL['inform'],
+                               sender=AgenteBusqueda.uri,
+                               msgcnt=mss_cnt,
+                               receiver=msgdic['sender'], )
+    mss_cnt += 1
+
+    logger.info('Respondemos a la peticion')
+
+    return gr.serialize(format='xml')
+
 
 
 @app.route("/Stop")
@@ -135,7 +189,20 @@ if __name__ == '__main__':
     # Ponemos en marcha los behaviors
     ab1 = Process(target=agentbehavior1, args=(cola1,))
     ab1.start()
+    # Registramos el solver en el servicio de directorio
+    solveradd = 'http://'+hostaddr+':'+str(port)
+    solverid = hostaddr.split('.')[0] + '-' + str(port)
+    mess = 'REGISTER|'+solverid+',BUSQUEDA,'+solveradd
+    done = False
+    while not done:
+        try:
+            resp = requests.get(diraddress + '/message', params={'message': mess}).text
+            done = True
+        except ConnectionError:
+            pass
 
+    if 'OK' in resp:
+        print(f'SOLVER {solverid} successfully registered')
     # Ponemos en marcha el servidor
     app.run(host=hostname, port=port)
 
