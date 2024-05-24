e591bbf3646cb64eb1e97ee0950a711be762ee77
XinXZP <xinxiangzhou4@gmail.com>
2024-05-24 17:51:03 +0200

    bases de datos pedido, transportista

--- implementacion/AgenteCentroLogistico.py
+++ implementacion/AgenteCentroLogistico.py
@@ -31,12 +31,16 @@ from AgentUtil.Agent import Agent
 from AgentUtil.Util import gethostname
 from AgentUtil.ACLMessages import *
 from docs.ecsdi import ECSDI
+from rdflib.namespace import XSD
 
 __author__ = 'javier'
 
 # Configuration stuff
 hostname = socket.gethostname()
-port = 9011
+port = 9013
+
+#Peso maximo de un lote
+MaxPesoLote = 100
 
 agn = Namespace("http://www.agentes.org#")
 
@@ -57,12 +61,21 @@ DirectoryAgent = Agent('DirectoryAgent',
                        'http://%s:9000/Stop' % hostname)
 
 # Global triplestore graph
-dsgraph = Graph()
+g = Graph()
 
 cola1 = Queue()
 
 
 app = Flask(__name__)
+
+#negociar Transportista
+#def negociarTransportista():
+
+#    return idTransportista
+
+#Creacion de lotes
+#def crearLotes():
+
 
 
 @app.route("/comm")
@@ -76,7 +89,62 @@ def comunicacion():
 
     message = request.args.get('content', '')  #
     print(message)
+    gm = Graph()
+    gm.parse(data=message, format='xml') 
+    msgdic = get_message_properties(gm)
 
+    gr = None
+
+    if msgdic is None:
+        # Si no es, respondemos que no hemos entendido el mensaje
+        gr = build_message(Graph(), ACL['not-understood'], sender=AgenteCompra.uri, msgcnt=get_count())
+    else:
+        if msgdic['performative'] != ACL.request:
+            # Si no es un request, respondemos que no hemos entendido el mensaje
+            gr = build_message(Graph(),
+                               ACL['not-understood'],
+                               sender=DirectoryAgent.uri,
+                               msgcnt=get_count())
+        else:
+            perf = msgdic['performative']
+
+            if perf != ACL.request:
+                # Si no es un request, respondemos que no hemos entendido el mensaje
+                gr = build_message(Graph(), ACL['not-understood'], sender=AgenteCompra.uri, msgcnt=get_count())
+            
+            else:
+                receiver_uri = msgdic['receiver'] #receiver_uri
+                # Averiguamos el tipo de la accion
+                accion = gm.value(subject=receiver_uri, predicate=RDF.type)
+
+                if accion == ECSDI.Pedido:
+                    #crear pedido
+                    g = Graph()
+                    # Definir el namespace de tu ontología ECSDI
+                    ECSDI = Namespace("urn:webprotege:ontology:ed5d344b-0a9b-49ed-9f57-1677bc1fcad8")
+
+                    # Definir la URI de tu pedido
+                    pedido_uri = ECSDI['Pedido/1']
+
+                    # Añadir triples al grafo
+                    g.add((pedido_uri, RDF.type, ECSDI.Pedido))
+                    g.add((pedido_uri, ECSDI.id, Literal(1, datatype=XSD.integer)))  # Id del pedido
+                    g.add((pedido_uri, ECSDI.latitud, Literal(10, datatype=XSD.float)))  # Latitud
+                    g.add((pedido_uri, ECSDI.longitud, Literal(20, datatype=XSD.float)))  # Longitud
+                    g.add((pedido_uri, ECSDI.metodoPago, Literal("tarjeta")))  # Método de pago
+                    g.add((pedido_uri, ECSDI.prioridadEntrega, Literal(1, datatype=XSD.integer)))  # Prioridad de entrega
+
+                    # Definir la URI de la compra asociada al pedido
+                    compra_uri = ECSDI['Compra/1']
+                    g.add((pedido_uri, ECSDI.compra_a_enviar, compra_uri))
+
+                    # Serializar el grafo en formato Turtle y guardarlo en un archivo
+                    with open("pedido.ttl", "w") as f:
+                        f.write(g.serialize(format="turtle").decode("utf-8"))
+                # Preparar lotes para enviar
+                #else: 
+                #    crearLotes()
+                
     return "p"
 
 
@@ -115,4 +183,4 @@ if __name__ == '__main__':
 
     # Wait for the behaviors to finish
     ab1.join()
-    print('The End')
+    #print('The End')
